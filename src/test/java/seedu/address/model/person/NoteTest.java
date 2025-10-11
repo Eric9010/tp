a45d2e300d122e9258a321e2810b56e2b24f3a3b
Charlize <E1354638@u.nus.edu.sg>
2025-10-12 02:34:08 +0800

    Add Note Test

---
+++ src/test/java/seedu/address/model/person/NoteTest.java
@@ -0,0 +1,34 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class NoteTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Note(null));
+    }
+
+    @Test
+    public void equals() {
+        Note note = new Note("Valid Note");
+
+        // same values -> returns true
+        assertTrue(note.equals(new Note("Valid Note")));
+
+        // same object -> returns true
+        assertTrue(note.equals(note));
+
+        // null -> returns false
+        assertFalse(note.equals(null));
+
+        // different types -> returns false
+        assertFalse(note.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(note.equals(new Note("Other Valid Note")));
+    }
+}
