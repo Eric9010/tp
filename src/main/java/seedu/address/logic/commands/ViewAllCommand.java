3751b5df05db296eb844d701a57d05110b9e2b72
NelsonWee <Nelsonweena@gmail.com>
2025-10-15 12:10:16 +0800

    Add ViewAllCommand

---
+++ src/main/java/seedu/address/logic/commands/ViewAllCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import javafx.collections.ObservableList;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ViewAllCommand extends Command {
+    public static final String COMMAND_WORD = "viewall";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Views all persons in the address book with complete details.\n"
+            + "Example: " + COMMAND_WORD;
+    public static final String MESSAGE_SUCCESS = "Viewing all persons:\n\n";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        // Update filter to show all persons
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        // Get all persons
+        ObservableList<Person> persons = model.getFilteredPersonList();
+
+        // Build detailed output message
+        StringBuilder output = new StringBuilder(MESSAGE_SUCCESS);
+
+        if (persons.isEmpty()) {
+            output.append("No contacts found in address book.");
+        } else {
+            output.append(String.format("Total contacts: %d\n\n", persons.size()));
+
+            int index = 1;
+            for (Person person : persons) {
+                output.append(String.format("%d. ", index));
+                output.append(Messages.format(person));
+                output.append("\n\n");
+                index++;
+            }
+        }
+
+        return new CommandResult(output.toString());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // All ViewAllCommand instances are equal
+        return other == this || (other instanceof ViewAllCommand);
+    }
+}
+
