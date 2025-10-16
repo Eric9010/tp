27d6682a68a65c586f7069660c4c68272298ba6c
EricWei <194746012+Eric9010@users.noreply.github.com>
2025-10-16 20:21:10 +0800

    Fix sample data format

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Event;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Event;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -21,16 +21,20 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         Person alex = new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Google"), getTagSet("Collegue"));
-        alex.addEvent(new Event("Team Lunch", "2025-10-20", "12:00", "F2F", "Google Office", "Lunch with Alex"));
-        alex.addEvent(new Event("Project Deadline", "2025-11-01", "17:00", null, "Workstation", "Submit report for Alex"));
+        alex.addEvent(new Event("Team Lunch", "2025-10-20", "12:00", "F2F", "Google Office",
+                "Lunch with Alex"));
+        alex.addEvent(new Event("Project Deadline", "2025-11-01", "17:00", null, "Workstation",
+                "Submit report for Alex"));
 
         Person bernice = new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Meta"), getTagSet("Interviewer", "Recruiter"));
-        bernice.addEvent(new Event("Interview", "2025-10-18", "15:00", "ZOOM", "Online", "Super important interview"));
+        bernice.addEvent(new Event("Interview", "2025-10-18", "15:00", "ZOOM", "Online",
+                "Super important interview"));
 
         Person charlotte = new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
                 new Email("charlotte@example.com"), new Address("NUS"), getTagSet("Friend"));
-        charlotte.addEvent(new Event("Coffee Chat", "2025-10-19", "12:00", null, "Starbucks @ Vivo", null));
+        charlotte.addEvent(new Event("Coffee Chat", "2025-10-19", "12:00", null, "Starbucks @ Vivo",
+                null));
 
         return new Person[] {
             alex, bernice, charlotte
