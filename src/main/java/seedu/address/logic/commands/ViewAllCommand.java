547df62e0d6603f0d0ace047cac85a1e09e48c0e
Teh Xin Hui <158593330+GREYHAT262@users.noreply.github.com>
2025-10-16 19:53:22 +0800

    Modify ViewAllCommand and remove list function

--- src/main/java/seedu/address/logic/commands/ViewAllCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewAllCommand.java
@@ -5,7 +5,6 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import javafx.collections.ObservableList;
 import seedu.address.model.Model;
-import seedu.address.model.person.Event;
 import seedu.address.model.person.Person;
 
 /**
@@ -16,7 +15,7 @@ public class ViewAllCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Views all persons in the address book with complete details.\n"
             + "Example: " + COMMAND_WORD;
-    public static final String MESSAGE_SUCCESS = "Viewing all persons:\n\n";
+    public static final String MESSAGE_SUCCESS = "Viewing all persons\n";
 
     @Override
     public CommandResult execute(Model model) {
@@ -34,34 +33,7 @@ public class ViewAllCommand extends Command {
         if (persons.isEmpty()) {
             output.append("No contacts found in address book.");
         } else {
-            output.append(String.format("Total contacts: %d\n\n", persons.size()));
-
-            int index = 1;
-            for (Person person : persons) {
-                output.append(String.format("%d. Name: %s\n", index, person.getName()));
-                output.append(String.format("   Phone: %s\n", person.getPhone()));
-                output.append(String.format("   Email: %s\n", person.getEmail()));
-                output.append(String.format("   Address: %s\n", person.getAddress()));
-
-                //tags
-                output.append("   Tags: ");
-                person.getTags().forEach(tag -> output.append(tag).append(" "));
-                output.append("\n");
-
-                //events
-                if (person.getEvents() != null && !person.getEvents().isEmpty()) {
-                    output.append("   Events:\n");
-                    int eventIndex = 1;
-                    for (Event event : person.getEvents()) {
-                        output.append(String.format("      %d) %s\n", eventIndex, event));
-                        eventIndex++;
-                    }
-                } else {
-                    output.append("   Events: (none)\n");
-                }
-                output.append("\n\n");
-                index++;
-            }
+            output.append(String.format("Total contacts: %d", persons.size()));
         }
 
         return new CommandResult(output.toString());
