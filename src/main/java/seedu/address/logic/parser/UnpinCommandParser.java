85c4033496fd3eab2f28c12c2008c3d454e30f37
EricWei <194746012+Eric9010@users.noreply.github.com>
2025-10-21 03:11:58 +0800

    Add whitespace for formatting

--- src/main/java/seedu/address/logic/parser/UnpinCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnpinCommandParser.java
@@ -25,5 +25,4 @@ public class UnpinCommandParser implements Parser<UnpinCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnpinCommand.MESSAGE_USAGE), pe);
         }
     }
-
 }
