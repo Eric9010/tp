1366d945db70bb36062ddc5f58bc538b2da73774
EricWei <194746012+Eric9010@users.noreply.github.com>
2025-10-21 04:20:33 +0800

    Update tests to use new constructor

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,11 +3,7 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -26,6 +22,8 @@ public class PersonBuilder {
     private Email email;
     private Address address;
     private Set<Tag> tags;
+    private Set<Event> events = new HashSet<>();
+    private Long pinTimestamp;
 
     /**
      * Creates a {@code PersonBuilder} with the default details.
@@ -36,6 +34,8 @@ public class PersonBuilder {
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
         tags = new HashSet<>();
+        events = new HashSet<>();
+        pinTimestamp = null;
     }
 
     /**
@@ -47,6 +47,8 @@ public class PersonBuilder {
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
         tags = new HashSet<>(personToCopy.getTags());
+        pinTimestamp = personToCopy.getPinTimestamp();
+        events = new HashSet<>(personToCopy.getEvents());
     }
 
     /**
@@ -90,7 +92,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags, null, new HashSet<>());
+        return new Person(name, phone, email, address, tags, pinTimestamp, events);
     }
 
 }
