cb15ba95f5175f9797cb3b1b783e37c8b6c06e50
NelsonWee <Nelsonweena@gmail.com>
2025-10-22 02:31:23 +0800

    Add TagCategories for industry tag detection

---
+++ src/main/java/seedu/address/ui/TagCategories.java
@@ -0,0 +1,27 @@
+package seedu.address.ui;
+
+import java.util.Set;
+
+/**
+ * Utility for tag industries on the UI layer
+ */
+public class TagCategories {
+    public static final Set<String> INDUSTRY_TAGS = Set.of(
+            "tech", "finance", "fintech", "accounting",
+            "consulting", "healthcare", "biotech", "pharma",
+            "education", "government", "public sector",
+            "energy", "utilities", "manufacturing",
+            "media", "advertising", "marketing",
+            "real estate", "property",
+            "retail", "ecommerce",
+            "logistics", "supplychain",
+            "travel", "hospitality", "airlines",
+            "nonprofit"
+    );
+
+    private TagCategories() {}
+
+    public static boolean isIndustry(String tagName) {
+        return INDUSTRY_TAGS.contains(tagName.toLowerCase());
+    }
+}
