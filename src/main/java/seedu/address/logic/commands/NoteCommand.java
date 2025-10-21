2a6d4661ba2047a68f53fef35d2e7b6b19578c55
Charlize <E1354638@u.nus.edu.sg>
2025-10-21 19:03:10 +0800

    Update NoteCommand class to support Person class

--- src/main/java/seedu/address/logic/commands/NoteCommand.java
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -1,9 +1,16 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Note;
+import seedu.address.model.person.Person;
+
+import java.util.List;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 public class NoteCommand extends Command {
     public static final String COMMAND_WORD = "note";
@@ -13,18 +20,45 @@ public class NoteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
+    public static final String MESSAGE_ADD_NOTE_SUCCESS = "Added note to Person: %1$s";
+    public static final String MESSAGE_DELETE_NOTE_SUCCESS = "Removed note from Person: %1$s";
+
     private final Index index;
-    private final String note;
+    private final Note note;
     
     public NoteCommand(Index index, String note) {
         requireAllNonNull(index, note);
 
         this.index = index;
-        this.note = note;
+        this.note = new Note(note);
     }
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("hello from note");
+    public CommandResult execute(Model model) throws CommandException {
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = new Person(
+                personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), note, personToEdit.getTags());
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether
+     * the remark is added to or removed from
+     * {@code personToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !note.toString().isEmpty() ? MESSAGE_ADD_NOTE_SUCCESS : MESSAGE_DELETE_NOTE_SUCCESS;
+        return String.format(message, Messages.format(personToEdit));
     }
 
     @Override
