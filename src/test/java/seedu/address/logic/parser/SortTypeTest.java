fd88d8b3f63f63441bde6dc6d848178abff1ff4c
Charlize <E1354638@u.nus.edu.sg>
2025-10-29 12:02:05 +0800

    Add SortTypeTest

--- src/test/java/seedu/address/logic/parser/SortTypeTest.java
+++ src/test/java/seedu/address/logic/parser/SortTypeTest.java
@@ -1,5 +1,22 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.parser.SortType.isValidSortType;
+
+import org.junit.jupiter.api.Test;
+
 public class SortTypeTest {
-    
+
+    @Test
+    public void isValidSortType_validSortType_true() {
+        assertTrue(isValidSortType("name"));
+        assertTrue(isValidSortType("timestamp"));
+        assertTrue(isValidSortType("NaMe"));
+    }
+
+    @Test
+    public void isValidSortType_invalidSortType_false() {
+        assertFalse(isValidSortType("email"));
+    }
 }
