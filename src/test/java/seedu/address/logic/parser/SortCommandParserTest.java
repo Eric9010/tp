729d3d0e601618d3d9972555f0b46f17113033f9
Charlize <E1354638@u.nus.edu.sg>
2025-10-30 23:06:21 +0800

    Add more test cases for SortCommandParserTest

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -26,10 +26,36 @@ public class SortCommandParserTest {
     }
 
     @Test
+    public void parse_withSpaces_success() {
+        String userInput = "   timestamp   ";
+        SortCommand expectedCommand = new SortCommand(SortType.TIMESTAMP);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_mixedCase_success() {
+        String userInput = "NaMe";
+        SortCommand expectedCommand = new SortCommand(SortType.NAME);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
     public void parse_missingCompulsoryField_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE);
-
-        // no parameters
         assertParseFailure(parser, SortCommand.COMMAND_WORD, expectedMessage);
+    }
+
+    @Test
+    public void parse_invalidSortType_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, SortCommand.COMMAND_WORD + " time", expectedMessage);
+        assertParseFailure(parser, SortCommand.COMMAND_WORD + " named", expectedMessage);
+        assertParseFailure(parser, SortCommand.COMMAND_WORD +" 123", expectedMessage);
+    }
+
+    @Test
+    public void parse_emptyInput_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, SortCommand.COMMAND_WORD + "   ", expectedMessage);
     }
 }
