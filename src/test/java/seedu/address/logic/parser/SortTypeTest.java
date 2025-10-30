278c1140f7a6e48bb1c9f49ea0ce8ba5f59d7791
Charlize <E1354638@u.nus.edu.sg>
2025-10-30 23:01:29 +0800

    Add more test cases for SortTypeTest

--- src/test/java/seedu/address/logic/parser/SortTypeTest.java
+++ src/test/java/seedu/address/logic/parser/SortTypeTest.java
@@ -2,21 +2,30 @@ package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.parser.SortType.isValidSortType;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class SortTypeTest {
 
     @Test
-    public void isValidSortType_validSortType_true() {
-        assertTrue(isValidSortType("name"));
-        assertTrue(isValidSortType("timestamp"));
-        assertTrue(isValidSortType("NaMe"));
-    }
+    public void isValidSortType() {
+        //null
+        assertThrows(NullPointerException.class, () -> SortType.isValidSortType(null));
 
-    @Test
-    public void isValidSortType_invalidSortType_false() {
-        assertFalse(isValidSortType("email"));
+        //empty spaces
+        assertFalse(SortType.isValidSortType(""));
+        assertFalse(SortType.isValidSortType(" "));
+
+        //invalid
+        assertFalse(SortType.isValidSortType("time")); //substring
+        assertFalse(SortType.isValidSortType("named")); // superstring
+        assertFalse(SortType.isValidSortType("123"));
+        assertFalse(SortType.isValidSortType("!?}"));
+
+        //valid
+        assertTrue(SortType.isValidSortType("name"));
+        assertTrue(SortType.isValidSortType("timestamp"));
+        assertTrue(SortType.isValidSortType("NaMe"));
     }
 }
